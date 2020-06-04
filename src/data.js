075d159282386a141cc6fddfd729ce5696ae4f6f
Dpessheva <dpessheva@gmail.com>
2020-06-04 19:18:48 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,99 @@
+const data = [
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#FFDB14'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#e17b77'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#1DA1F2'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#018f69'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    }, {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#FFDB14'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#e17b77'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#1DA1F2'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    },
+    {
+        text: "A short description of an event",
+        date: "April 09 2020",
+        category: {
+            tag: "other",
+            color: '#018f69'
+        },
+        link: {
+            url: '',
+            text:"Read more"
+        }
+    }
+]
+
+export default data;
